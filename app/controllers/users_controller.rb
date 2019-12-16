6b056e17bc41a8532c9ea64b224a2ac443422d4e
ookidoshi <okidookido@gmail.com>
2019-12-16 14:19:18 +0900

    グループ編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,22 @@
 class UsersController < ApplicationController
 
-def edit
-end
-
-def update
-  if current_user.update(user_parrams)
-    redirect_to root_path
-  else
-    render :edit
+  def index
   end
-end
-
-private
-
-def user_params
-    params.require(:user).permit(:name, :email)
+    
+  def edit
   end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
