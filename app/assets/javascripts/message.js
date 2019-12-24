754440e580b9a7da08219c2e087d649f884e9876
ookidoshi <okidookido@gmail.com>
2019-12-24 21:03:42 +0900

    自動更新機能です！

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -61,5 +61,34 @@ $(function(){
         alert('メッセージ送信に失敗しました');
       });
       return false;
-  });
+    })
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $("form")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+  }
 });
+
